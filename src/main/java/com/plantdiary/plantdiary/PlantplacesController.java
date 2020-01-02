8872400a9211781b340aace2400da1a329ffa22e
promise <promiseokosun@gmail.com>
2020-01-03 00:57:21 +0100

    A quick update on the controller class

--- src/main/java/com/plantdiary/plantdiary/PlantplacesController.java
+++ src/main/java/com/plantdiary/plantdiary/PlantplacesController.java
@@ -2,12 +2,23 @@ package com.plantdiary.plantdiary;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PlantplacesController {
 
-	@RequestMapping("/start")
-	public String start(){ 
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read(){ 
+		return "start"; 
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers="{content-type=text/json}")
+	public String startJson(){ 
+		return "start"; 
+	}
+	
+	@RequestMapping(value="/start", params={"loyalty=blue"})
+	public String create(){ 
 		return "start"; 
 	}
 	
